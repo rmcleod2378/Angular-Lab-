dcee01b1bd414b13fe50dd2988ef7a3bf7d766e2
rmcleod2378 <44953292+rmcleod2378@users.noreply.github.com>
2019-02-10 19:02:55 -0500

    Add files via upload
    Updates on part 2

--- AngularLab/app.controller.js
+++ AngularLab/app.controller.js
@@ -1,24 +1,36 @@
 "use strict";
+// Used in a constructor to build the controller
 function ToDoController(){
+    // this references the object that the constructor returns
     const vm = this;
+    // tasks is a property of the controller
     vm.tasks = [
-        { task: "Walk the dog", completed: false},
-        { task: "Take out garbage", completed: true},
-        { task: "Wash the windows", completed: false},
-        { task: "Clean the fridge", completed: true}
+        { task: "Create Angular ToDoApp", completed: true},
+        { task: "Input Angular Tasks", completed: true},
+        { task: "Make ToDoApp Functional", completed: false},
+        { task: "Turn in Lab", completed: false}
     ]
-
-    vm.add = function(){
-
+    // add is a method that pushes a piece of data (the parameter) into the tasks array
+    // clears inputs by resetting the object called newTasks
+    vm.add = function(newTasks){
+        newTasks.completed = false
+        vm.newTasks.push(angular.copy(newTasks))
+        vm.newTasks = {}
     }
-    vm.remove = function(){
-
+    // removes the tasks from the array
+    // takes the index (parameter) and passes it to the splice
+    //method to remove an element from tasks
+    vm.remove = function(index){
+        vm.tasks.splice(index, 1);
     }
-    vm.completed = function(){
-        
+    // sets the property completed to true
+    vm.completed = function(index){
+        vm.tasks[index].completed = true
     }
-};
-
+}
+// getter syntax to retrieve the module called todoApp
 angular
+// links module
 .module("todoApp")
+//builds controller, the 1st argument is the name of the controller. the 2nd argument is the function reference
 .controller("ToDoController", ToDoController);
--- AngularLab/app.module.js
+++ AngularLab/app.module.js
@@ -1,2 +1,8 @@
 "use strict";
+// setting/creating a module
+// the first argument is the name of the module
+// the second argument is an array of dependencies
 angular.module("todoApp", []);
+// modules contain all of the pieces of our application
+// you can have more than 1 module
+// to use it, you reference the module in the <html> element using ngApp
